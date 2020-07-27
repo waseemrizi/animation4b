1c51b444eb818d8aec3f072b95ad8aa8441a08ef
waseemrizi <waseemrizi@outlook.com>
2020-07-27 11:58:39 +0500

    adding files project 4b

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import { Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+
+
+
+function App() {
+  
+    return (
+    <>
+      <Header />
+      <Home />
+            
+    </>
+  );
+}
+
+export default App;
